11ebe9192130dc14065e9c39546b61982c621895
vivy9524 <ochiengvianne98@gmail.com>
2022-07-13 11:15:22 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,14 @@
 void print_rev(char *s)
 {
 int i, n;
-n = strlen(s);
-for (i = n-1; i >= 0; i--)
+
+n = 0;
+while (s[n] != '\0')
+	n++;
+
+for (i = n - 1; i >= 0; i--)
 {
 	_putchar(s[i]);
 }
-
+_putchar('\n');
 }
