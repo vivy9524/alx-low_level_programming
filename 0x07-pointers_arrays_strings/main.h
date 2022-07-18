4b15f6686050547988bbf61b4d10c85a0c86ada2
vivy9524 <ochiengvianne98@gmail.com>
2022-07-18 13:02:04 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,5 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
 
 #endif
