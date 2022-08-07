319ede62d443547166e2fff1e5b6ccdb168a3315
vivy9524 <ochiengvianne98@gmail.com>
2022-08-07 03:16:35 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,14 +4,13 @@
  * print_name - prints a name
  * @name: The name to be printed
  * @f: A pointer to a function that prints a name
+ *
+ * Return - 0
  */
 void print_name(char *name, void (*f)(char *))
 {
 
-	if (name == NULL || f == NULL)
-
-		return;
-
-	f(name);
+	if (name && f)
+		f(name);
 
 }
