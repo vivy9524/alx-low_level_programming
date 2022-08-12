e5358f0837fc18317d46475b8e729c23b41e4f3e
vivy9524 <ochiengvianne98@gmail.com>
2022-08-11 22:06:15 -0700

    task 1

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -28,10 +28,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-void myStartupFun(void) __attribute__ ((constructor));
-void myConstructor(void);
+
 
 #endif 
